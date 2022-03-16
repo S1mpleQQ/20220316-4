8300110fcc063a7652e19d99972f54a6aaefc15b
S1mpleQQ <c110196116@nkust.edu.tw>
2022-03-16 17:14:20 +0800

    加入專案檔案。

---
+++ 20220316-4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220316_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            for (int i = 10; i>0; i -= 2)
+            {
+                if (i == 3) continue;
+                Console.Write("{0}", i);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
